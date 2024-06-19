437c1c9ef35cb6805b8f277217ee606c629c1872
Francisco Lopez <lopezf28@gmail.com>
2024-06-18 22:48:26 -0400

    creando el Componente del Contador

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>{{ counter }}</p>
+        <button @click="increment">Incrementar</button>
+        <button @click="decrement">Decrementar</button>
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+    computed: {
+        ...mapState(['counter'])
+    },
+    methods: {
+        ...mapMutations(['increment', 'decrement'])
+    }
+};
+</script>
