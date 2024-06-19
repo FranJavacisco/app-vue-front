4fd3773e8bcaedc3c131ea26c8c00d9fadf6d19f
Francisco Lopez <lopezf28@gmail.com>
2024-06-18 23:20:00 -0400

    Creando Componente Parent

---
+++ src/components/Parent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Child @message="displayMessage" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import Child from './Child.vue';
+
+export default {
+    components: { Child },
+    data() {
+        return { message: '' };
+    },
+    methods: {
+        displayMessage(msg) {
+            this.message = msg;
+        }
+    }
+};
+</script>
