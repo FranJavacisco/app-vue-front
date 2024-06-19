f1e7a8905858beba07a2e23f87eb0810f2be98a3
Francisco Lopez <lopezf28@gmail.com>
2024-06-18 23:22:33 -0400

    Creando Componente Child

---
+++ src/components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input v-model="input" />
+        <button @click="sendMessage">Enviar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { input: '' };
+    },
+    methods: {
+        sendMessage() {
+            this.$emit('message', this.input);
+        }
+    }
+};
+</script>
